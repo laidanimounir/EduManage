22b99ebf4968f654f0b6b6af6f6b83d51784d949
laidanimounir <laidanimounir606@gmail.com>
2026-07-22 04:26:41 +0100

    add app constants for theme and configuration

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  AppConstants._();
+
+  static const String appName = 'EduManage';
+  static const String defaultLocale = 'ar';
+  static const String currencySymbol = 'DA';
+  static const String currencyCode = 'DZD';
+  static const int defaultSessionTimeoutMinutes = 30;
+  static const int checkInFeedbackDurationSeconds = 3;
+  static const int defaultYear = 2026;
+}
