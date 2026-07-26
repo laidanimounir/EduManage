a6da3a1f7e477fde5b4c327bbd0fa14f4924e757
laidanimounir <laidanimounir606@gmail.com>
2026-07-26 03:42:25 +0100

    apply darker near-black palette (BUG 8): chromeBase #0B0D09, chromeSurface #12140F, chromeBorder #1E201A, contentBg #0E100C, contentCard #131510; cards have subtle lighter border on dark bg

--- lib/constants/theme_tokens.dart
+++ lib/constants/theme_tokens.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 class ShellTokens {
   ShellTokens._();
 
-  static const Color chromeBase = Color(0xFF121610);
-  static const Color chromeSurface = Color(0xFF1B1F18);
-  static const Color chromeBorder = Color(0xFF2A2E25);
+    static const Color chromeBase = Color(0xFF0B0D09);
+    static const Color chromeSurface = Color(0xFF12140F);
+    static const Color chromeBorder = Color(0xFF1E201A);
 
-  static const Color accent = Color(0xFF5B8C5A);
-  static const Color accentMuted = Color(0xFF3D5E3C);
+    static const Color accent = Color(0xFF5B8C5A);
+    static const Color accentMuted = Color(0xFF3D5E3C);
 
-  static const Color textPrimary = Color(0xFFE8EBE4);
-  static const Color textSecondary = Color(0xFF989C94);
-  static const Color textDisabled = Color(0xFF5B5F58);
+    static const Color textPrimary = Color(0xFFE8EBE4);
+    static const Color textSecondary = Color(0xFF989C94);
+    static const Color textDisabled = Color(0xFF5B5F58);
 }
 
 class SemanticTokens {
@@ -26,9 +26,9 @@ class SemanticTokens {
 class ContentTokens {
   ContentTokens._();
 
-  static const Color background = Color(0xFF161913);
-  static const Color card = Color(0xFF1E221B);
-  static const Color border = Color(0xFF2A2E25);
+    static const Color background = Color(0xFF0E100C);
+    static const Color card = Color(0xFF131510);
+    static const Color border = Color(0xFF1E201A);
 
   static const Color textPrimary = Color(0xFFE8EBE4);
   static const Color textSecondary = Color(0xFF989C94);
