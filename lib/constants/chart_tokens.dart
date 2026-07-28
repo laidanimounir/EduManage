11e267e3f376117e9eca6831433a2e7391f0786e
laidanimounir <laidanimounir606@gmail.com>
2026-07-28 05:20:06 +0100

    Added fl_chart dependency and ChartTokens design palette for dashboard charts

---
+++ lib/constants/chart_tokens.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+class ChartTokens {
+  ChartTokens._();
+
+  static const seriesPalette = [
+    Color(0xFF4A90D9),
+    Color(0xFF5B8C5A),
+    Color(0xFFE67E22),
+    Color(0xFF9B59B6),
+    Color(0xFFC2483D),
+    Color(0xFFF1C40F),
+    Color(0xFF1ABC9C),
+    Color(0xFFE74C3C),
+  ];
+
+  static const gridLine = Color(0xFF2A2A2A);
+  static const axisLabel = Color(0xFF888888);
+
+  static const tooltipBg = Color(0xFF1E1E1E);
+  static const tooltipText = Color(0xFFE0E0E0);
+
+  static const heatmapEmpty = Color(0xFF1A1A1A);
+  static const heatmapLow = Color(0xFF1B3A5C);
+  static const heatmapMedium = Color(0xFF2E6B8A);
+  static const heatmapHigh = Color(0xFFE67E22);
+  static const heatmapMax = Color(0xFFC0392B);
+
+  static const trendUp = Color(0xFF27AE60);
+  static const trendDown = Color(0xFFE74C3C);
+  static const trendNeutral = Color(0xFF888888);
+}
